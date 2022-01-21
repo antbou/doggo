f76ea86a682a844264bde39ac578a50820cac439
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2022-01-21 14:00:36 +0100

    Add edit dog view

--- src/app/src/main/java/com/example/whatthedog/fragment_edit_dog.kt
+++ src/app/src/main/java/com/example/whatthedog/fragment_edit_dog.kt
@@ -1,4 +1,4 @@
-package com.example.whatthedog.ui.login
+package com.example.whatthedog
 
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,7 +15,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-import com.example.whatthedog.databinding.FragmentRegisterDogBinding
 
 import com.example.whatthedog.R
 
@@ -26,10 +25,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [fragment_register_dog.newInstance] factory method to
+ * Use the [fragment_edit_dog.newInstance] factory method to
  * create an instance of this fragment.
  */
-class fragment_register_dog : Fragment() {
+class fragment_edit_dog : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -47,7 +46,7 @@ class fragment_register_dog : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_dog, container, false)
+        return inflater.inflate(R.layout.fragment_edit_dog, container, false)
     }
 
     companion object {
@@ -57,12 +56,12 @@ class fragment_register_dog : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_register_dog.
+         * @return A new instance of fragment fragment_edit_dog.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            fragment_register_dog().apply {
+            fragment_edit_dog().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
